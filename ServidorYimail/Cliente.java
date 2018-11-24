39019cedfa03119a6ba1ddffc6db185d0f4762bd
Jair1012 <jair.flores@galileo.edu>
2018-11-23 18:31:32 -0600

    jflores_1
    Se juntan ambas partes del servidor.

---
+++ ServidorYimail/Cliente.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.net.*;
+
+class Cliente{
+
+	public static void main(String[] args){
+	BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+	String leo;
+	 String ip = "localhost";
+	 Socket conexion = null;
+	 int PUERTO = 1400;
+
+		try{
+			conexion = new Socket(ip,PUERTO);
+			DataInputStream flujoDatosEntrada = new DataInputStream(conexion.getInputStream());
+			DataOutputStream flujoDatosSalida = new DataOutputStream(conexion.getOutputStream());//Creamos objeto para enviar
+			do{
+				System.out.println("ingrese informacion a enviar al servidor");
+				leo = buffer.readLine();
+				flujoDatosSalida.writeUTF(leo); 
+				System.out.println("Eco: " + flujoDatosEntrada.readUTF());
+			}while(!leo.equals("EXIT")); //Mandamos el mensaje al servidor
+
+		}catch(Exception e){
+
+			System.out.println("No se puedo crear la conexion");
+			e.printStackTrace();
+		}
+	}
+
+}
--- ServidorYimail/ServidorYimail.java
+++ ServidorYimail/ServidorYimail.java
@@ -1,207 +1,33 @@
-import java.util.*;
 import java.io.*;
+import java.net.*;
 
 public class ServidorYimail {
-
-    // sirve para printear lo que devuelva el server
-    public static void clientServer(String print) throws Exception {
-        System.out.print("SERVER : ");
-        System.out.println(print);
+    private final int puerto1 = 1400;
+//    private final int puerto2 = 1500;
+    private ServerSocket cs1;
+    private Socket sc1;
+//    private ServerSocket cs2;
+//    private Socket sc2;
+    private DataOutputStream salida;
+    public void inicia(){
+        DataInputStream entrada;
+        try{
+            cs1 = new ServerSocket(puerto1);
+            sc1 = new Socket();
+            System.out.println("Esperando conexion");
+            while(true){
+	            sc1 = cs1.accept();
+	            ServidorCliente cliente = new ServidorCliente(sc1);
+	            cliente.start();
+	        }
+        }catch(Exception e){
+            System.out.println("Ocurrio un error inesperado en N/");
+            System.out.println(e.getMessage());
+        }
     }
-
-    public static void main(String[] args) throws Exception {
-        String finish = "";
-        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-        DB myDb = new DB("servidorcorreo.db");
-
-        do {
-            System.out.print("CLIENT : ");
-            String informacion = buffer.readLine();
-            String[] valores = informacion.split(" ");
-            String accion = valores[0].trim();
-            switch (accion) {
-            case "VERIFYSERVER":
-                /*------------------------------------------------------------------------*/
-                String user_caption = "";
-                String servidor_caption = "";
-                if (valores.length == 3) {
-                    user_caption = valores[1].trim();
-                    servidor_caption = valores[2].trim();
-                }
-                if (!myDb.connect()) {
-                    clientServer("ERROR_DB_CONECTIONS");
-                } else {
-                    try {
-
-                        String query = "select count(*) as existe from servidor ";
-                        query += "where nombre = '" + servidor_caption + "';";
-                        myDb.executeQuery(query, "rs1");
-                        while (myDb.next("rs1")) {
-                            String respuesta = myDb.getString("existe", "rs1") + "";
-                            if (Integer.parseInt(respuesta) >= 1) {
-                                System.out.println("OK SERVER VERIFICATE");
-                            }
-                        }
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                    } finally {
-                        myDb.close();
-                    }
-                }
-                /*------------------------------------------------------------------------*/
-                break;
-            case "LOGIN":
-                /*------------------------------------------------------------------------*/
-                String user_caption_log = "";
-                String pasword_caption_log = "";
-                if (valores.length == 3) {
-                    user_caption_log = valores[1].trim();
-                    pasword_caption_log = valores[2].trim();
-                }
-                if (!myDb.connect()) {
-                    clientServer("ERROR_DB_CONECTIONS");
-                } else {
-                    try {
-                        String query = "select count(*) as existe from usuario ";
-                        query += "where idusuario = '" + user_caption_log + "';";
-                        myDb.executeQuery(query, "rs1");
-                        while (myDb.next("rs1")) {
-                            String respuesta = myDb.getString("existe", "rs1") + "";
-                            if (Integer.parseInt(respuesta) >= 1) {
-                                String query2 = "select count(*) as existe from usuario ";
-                                query2 += "where idusuario = '" + user_caption_log + "' and ";
-                                query2 += " password = '" + pasword_caption_log + "';";
-                                myDb.executeQuery(query2, "rsl2");
-                                while (myDb.next("rsl2")) {
-                                    String respuesta_2 = myDb.getString("existe", "rsl2") + "";
-                                    if (Integer.parseInt(respuesta_2) >= 1) {
-                                        boolean respuesta_3 = myDb.executeNonQuery(
-                                                "update usuario set estado=1 where idusuario='" + user_caption_log
-                                                        + "' and password='" + pasword_caption_log + "';");
-                                        if (respuesta_3) {
-                                            System.out.println("OK LOGIN");
-                                        } else {
-                                            System.out.println("LOGIN ERROR UNKNOWN");
-                                        }
-                                    } else {
-                                        System.out.println("LOGIN ERROR 102");
-                                    }
-                                }
-                            } else {
-                                System.out.println("LOGIN ERROR 101");
-                            }
-                        }
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                        System.out.println("LOGIN ERROR UNKNOWN");
-                    } finally {
-                        myDb.close();
-                    }
-                }
-                /*------------------------------------------------------------------------*/
-                break;
-            case "CLIST":
-                /*------------------------------------------------------------------------*/
-                String user_caption_log_list = "";
-                if (valores.length == 2) {
-                    user_caption_log_list = valores[1].trim();
-                }
-                if (!myDb.connect()) {
-                    clientServer("ERROR_DB_CONECTIONS");
-                } else {
-                    try {
-                        String query = "select count(*) as cantidad_contactos from contacto ";
-                        query += "where idusuario = '" + user_caption_log_list + "';";
-                        myDb.executeQuery(query, "rsl_cantidad");
-                        while (myDb.next("rsl_cantidad")) {
-                            String respuesta_3 = myDb.getString("cantidad_contactos", "rsl_cantidad") + "";
-                            if (Integer.parseInt(respuesta_3) > 0) {
-                                String query2 = "select * from contacto ";
-                                query2 += "where idusuario = '" + user_caption_log_list + "';";
-                                myDb.executeQuery(query2, "rsl_contactos");
-                                Integer cont_aux = 1;
-                                while (myDb.next("rsl_cantidad")) {
-                                    if (cont_aux.equals(Integer.parseInt(respuesta_3))) {
-
-                                        System.out.println("OK CLIST " + myDb.getString("idusuario", "rsl_contactos")
-                                                + "@" + myDb.getString("servidor", "rsl_contactos") + " *");
-                                    } else {
-                                        System.out.println("OK CLIST " + myDb.getString("idusuario", "rsl_contactos")
-                                                + "@" + myDb.getString("servidor", "rsl_contactos"));
-                                    }
-                                    cont_aux++;
-                                }
-                            } else {
-                                System.out.println("CLIST ERROR 103");
-                            }
-                        }
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                        System.out.println("CLIST ERROR UNKNOWN");
-                    } finally {
-                        myDb.close();
-                    }
-                }
-                /*------------------------------------------------------------------------*/
-                break;
-            case "GETNEWMAILS":
-                /*------------------------------------------------------------------------*/
-                String user_caption_log_mails = "";
-                if (valores.length == 2) {
-                    user_caption_log_mails = valores[1].trim();
-                }
-                if (!myDb.connect()) {
-                    clientServer("ERROR_DB_CONECTIONS");
-                } else {
-                    try {
-                        String query = "select count(*) as cantidad_correos from correo ";
-                        query += "where idusuarioremitente = '" + user_caption_log_mails + "';";
-                        myDb.executeQuery(query, "rsl_cantidad");
-                        while (myDb.next("rsl_cantidad")) {
-                            String respuesta_3 = myDb.getString("cantidad_correos", "rsl_cantidad") + "";
-                            if (Integer.parseInt(respuesta_3) > 0) {
-                                String query2 = "select correo.idusuarioremitente as sender, correo.asunto as asunto, correo.cuerpo as cuerpo"; 
-                                query2 += " from correo, destinatario where correo.idcorreo = destinatario.idcorreo ";
-                                query2 += "and destinatario.idusuarioreceptor = '" + user_caption_log_mails + "';";
-                                myDb.executeQuery(query2, "rsl_newmails");
-                                Integer cont_aux = 1;
-                                while (myDb.next("rsl_newmails")) {
-                                    if (cont_aux.equals(Integer.parseInt(respuesta_3))) {
-                                        System.out.println("OK GETNEWMAILS " + myDb.getString("sender", "rsl_newmails")
-                                                + " '" + myDb.getString("asunto", "rsl_newmails")
-                                                + "' '" + myDb.getString("cuerpo", "rsl_newmails") + "' *");
-                                    } else {
-                                        System.out.println("OK GETNEWMAILS " + myDb.getString("sender", "rsl_newmails")
-                                                + " '" + myDb.getString("asunto", "rsl_newmails")
-                                                + "' '" + myDb.getString("cuerpo", "rsl_newmails") + "'");
-                                    }
-                                    cont_aux++;
-                                }
-                            } else {
-                                System.out.println("OK GETNEWMAILS NOMAILS");
-                            }
-                        }
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                        System.out.println("GETNEWMAILS ERROR UNKNOWN");
-                    } finally {
-                        myDb.close();
-                    }
-                }
-                /*------------------------------------------------------------------------*/
-                break;
-            case "EXIT":
-                /*------------------------------------------------------------------------*/
-                finish = accion;
-                System.out.println("Saliendo...");
-                myDb.close();
-                /*------------------------------------------------------------------------*/
-                break;
-            default:
-                System.out.println("");
-                break;
-            }
-        } while (!finish.equals("EXIT"));
-
+    public static void main(String[] args) {
+       ServidorYimail io = new ServidorYimail();
+       io.inicia();
     }
+    
 }
