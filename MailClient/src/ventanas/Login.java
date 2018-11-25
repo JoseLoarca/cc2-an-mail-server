d3e030366c5da59add775f082580cf4021e95474
harimpalma <45084147+harimpalma@users.noreply.github.com>
2018-11-24 22:15:19 -0600

    test xd

--- MailClient/src/ventanas/Login.java
+++ MailClient/src/ventanas/Login.java
@@ -161,15 +161,7 @@ public class Login extends javax.swing.JFrame {
                             case "SERVER : OK LOGIN":
                                     Interfaz menu = new Interfaz();
                                     menu.setUsername(user);
-                                    
-                                    
-                                    
-                                    
-                                    
-                                    
-                                    
-                                    
-                                    
+                        
                                     menu.setVisible(true);
                                     this.setVisible(false);
                                 break;
