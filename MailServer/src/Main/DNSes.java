a36c586a6759497ee3b2c0d1f040111c9c4b7c5f
Manuel Santos <manuel.santos@galileo.edu.gt>
2018-11-26 18:05:13 -0600

    Se suben cambios

--- MailServer/src/Main/DNSes.java
+++ MailServer/src/Main/DNSes.java
@@ -58,12 +58,57 @@ public class DNSes extends Thread {
 
     public void GETIPTABLE() {
         try {
+            DB myDb2 = new DB("servidorcorreo.db");
             conexion = new Socket(ip, puerto);
             DataInputStream flujoDatosEntrada = new DataInputStream(conexion.getInputStream());
             DataOutputStream flujoDatosSalida = new DataOutputStream(conexion.getOutputStream());
             leo = "GETIPTABLE";
             flujoDatosSalida.writeUTF(leo);
-            System.out.println(flujoDatosEntrada.readUTF());
+
+            String ips = flujoDatosEntrada.readUTF();
+            String[] valores = ips.split(" ");
+
+            try {
+                if (!myDb2.connect()) {
+                    System.out.println("ERROR DB CONNECT");
+                } else {
+                    if (valores.length == 5 && valores[0].equals("OK") && valores[1].equals("IPTABLE") && valores[4].equals("*")) {
+                        String query_delete = "delete from servidor where lower(nombre) != 'yimail';";
+                        boolean respuesta_delete = myDb2.executeNonQuery(query_delete);
+                        if (respuesta_delete) {
+                            if (!valores[2].equals("yimail") || !valores[2].equals("Yimail")) {
+                                String query_insert_newcont = "insert into contacto (idusuariocontacto,ip,estado)";
+                                query_insert_newcont += "values(" + valores[2] + "," + valores[3]
+                                        + ",1);";
+                                boolean respuesta_7 = myDb2.executeNonQuery(query_insert_newcont);
+                                if (respuesta_7) {
+                                    System.out.println("IPS OBTENIDA");
+                                }
+                            }
+                        }
+                    } else if (valores.length == 4 && valores[0].equals("OK") && valores[1].equals("IPTABLE")) {
+                        boolean bandera = true;
+
+                        String query_delete = "delete from servidor where lower(nombre) != 'yimail';";
+                        boolean respuesta_delete = myDb2.executeNonQuery(query_delete);
+                        String query_insert_newcont = "insert into contacto (idusuariocontacto,ip,estado)";
+                        query_insert_newcont += "values(" + valores[2] + "," + valores[3] + ",1);";
+                        boolean respuesta_7 = myDb2.executeNonQuery(query_insert_newcont);
+                        if (respuesta_delete) {
+                            while (bandera) {
+                                String ips2 = flujoDatosEntrada.readUTF();
+                                String[] valores2 = ips2.split(" ");
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                System.out.println("NOOP ERROR UNKNOWN");
+            } finally {
+                myDb2.close();
+            }
+
             conexion.close();
         } catch (Exception e) {
             System.out.println("No se puedo crear la conexion");
